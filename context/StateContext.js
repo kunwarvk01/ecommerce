1fcae80747531522ec9c60b1195642ee3a53fddf
kunwarvk01 <kunwarvk01@gmail.com>
2022-11-02 10:22:00 +0530

    added cart

--- context/StateContext.js
+++ context/StateContext.js
@@ -6,32 +6,77 @@ const Context = createContext();
 export const StateContext = ({children}) => {
    const [showCart, setShowCart] = useState(false);
    const [cartItems, setCartItems] = useState([]);
-   const [totalPrice, setTotalPrice] = useState();
+   const [totalPrice, setTotalPrice] = useState(0);
    const [totalQuantities, setTotalQuantities] = useState(0);
    const [qty, setQty] = useState(1);
 
-   const onAdd = (product, quanity) => {
+   let foundProduct;
+   let index;
+
+   const onAdd = (product, quantity) => {
       const checkProductInCart = cartItems.find(
          item => item._id === product._id
       );
 
-      setTotalPrice(prevTotalPrice => prevTotalPrice + product.price * quanity);
-      setTotalQuantities(prevTotalQuantities => prevTotalQuantities + quanity);
+      setTotalPrice(
+         prevTotalPrice => prevTotalPrice + product.price * quantity
+      );
+      setTotalQuantities(prevTotalQuantities => prevTotalQuantities + quantity);
 
       if (checkProductInCart) {
          const updatedCartItems = cartItems.map(cartProduct => {
             if (cartProduct._id === product._id)
-               return {...cartProduct, quanity: cartProduct.quanity + quanity};
+               return {
+                  ...cartProduct,
+                  quantity: cartProduct.quantity + quantity
+               };
          });
-
          setCartItems(updatedCartItems);
       } else {
-         product.quanity = quanity;
-
+         product.quantity = quantity;
          setCartItems([...cartItems, {...product}]);
       }
-
       toast.success(`${qty} ${product.name} added to the cart`);
+   };
+
+   const onRemove = product => {
+      foundProduct = cartItems.find(item => item._id === product._id);
+      const newCartItems = cartItems.filter(item => item._id !== product._id);
+
+      setTotalPrice(
+         prevTotalPrice =>
+            prevTotalPrice - foundProduct.price * foundProduct.quantity
+      );
+      setTotalQuantities(
+         prevTotalQuantities => prevTotalQuantities - foundProduct.quantity
+      );
+      setCartItems(newCartItems);
+   };
+
+   const toggleCartItemQuantity = (id, value) => {
+      foundProduct = cartItems.find(item => item._id === id);
+      index = cartItems.findIndex(product => product._id === id);
+
+      const newCartItems = cartItems.filter(item => item._id !== id);
+      if (value === "inc") {
+         setCartItems([
+            ...newCartItems,
+            {...foundProduct, quantity: foundProduct.quantity + 1}
+         ]);
+         setTotalPrice(prevTotalPrice => prevTotalPrice + foundProduct.price);
+         setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1);
+      } else if (value === "dec") {
+         if (foundProduct.quantity > 1) {
+            setCartItems([
+               ...newCartItems,
+               {...foundProduct, quantity: foundProduct.quantity - 1}
+            ]);
+            setTotalPrice(
+               prevTotalPrice => prevTotalPrice - foundProduct.price
+            );
+            setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1);
+         }
+      }
    };
 
    const incQty = () => {
@@ -56,7 +101,9 @@ export const StateContext = ({children}) => {
             qty,
             incQty,
             decQty,
-            onAdd
+            onAdd,
+            toggleCartItemQuantity,
+            onRemove
          }}
       >
          {children}
--- pages/index.js
+++ pages/index.js
@@ -4,30 +4,29 @@ import {client} from "../lib/client";
 import {Product, HeroBanner, FooterBanner} from "../components";
 
 const Home = ({products, bannerData}) => (
-    <>
-        <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
-        {console.log(bannerData)}
-        <div className="products-heading">
-            <h2>Best Selling Products</h2>
-            <p>Speakers of many variations</p>
-        </div>
-        <div className="products-container">
-            {products?.map(product => (
-                <Product key={product._id} product={product} />
-            ))}
-        </div>
-        <FooterBanner footerBanner={bannerData && bannerData[0]} />
-    </>
+   <>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      <div className="products-heading">
+         <h2>Best Selling Products</h2>
+         <p>Speakers of many variations</p>
+      </div>
+      <div className="products-container">
+         {products?.map(product => (
+            <Product key={product._id} product={product} />
+         ))}
+      </div>
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
+   </>
 );
 
 export const getServerSideProps = async () => {
-    const query = '*[_type=="product"]';
-    const products = await client.fetch(query);
+   const query = '*[_type=="product"]';
+   const products = await client.fetch(query);
 
-    const bannerQuery = '*[_type=="banner"]';
-    const bannerData = await client.fetch(bannerQuery);
+   const bannerQuery = '*[_type=="banner"]';
+   const bannerData = await client.fetch(bannerQuery);
 
-    return {props: {products, bannerData}};
+   return {props: {products, bannerData}};
 };
 
 export default Home;
