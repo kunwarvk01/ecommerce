75001413622ce120764398b0eb6d29a79263e8ac
kunwarvk01 <kunwarvk01@gmail.com>
2023-08-25 08:20:05 +0530

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,19 @@
 import React from "react";
-import {Toaster} from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 
-import {Layout} from "../components";
-import "../styles/globals.css";
-import {StateContext} from "../context/StateContext";
+import { Layout } from "../components";
+import "../styles/global.css";
+import { StateContext } from "../context/StateContext";
 
-function MyApp({Component, pageProps}) {
-   return (
-      <StateContext>
-         <Layout>
-            <Toaster />
-            <Component {...pageProps} />
-         </Layout>
-      </StateContext>
-   );
+function MyApp({ Component, pageProps }) {
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
 }
 
 export default MyApp;
